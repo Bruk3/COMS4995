8e84a768292104a594446c0cb1f0c1c0666fa97f
Bruk Zewdie <bruk@dyn-160-39-250-143.dyn.columbia.edu>
2020-10-08 16:11:03 -0400

    write yamlStringToJson, yamlFileToJson functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-const yaml = require('js-yaml');
+const yaml = require("js-yaml");
+const path = require("path");
+const fs = require("fs");
+
+function yamlStringToJson(target) {
+  let resultJson;
+  try {
+    resultJson = yaml.safeLoad(target);
+    console.log(`resultJson: ${JSON.stringify(resultJson, null, 2)}`);
+  } catch (e) {
+    console.log(`error parsing yaml string: ${e}`);
+    return null;
+  }
+  return resultJson;
+}
+
+async function yamlFileToJson(filename) {
+  const yamlString = fs.readFileSync(filename, "utf-8");
+  console.log(`PRINTING YAMLSTRING: ${yamlString}`);
+  console.log("End of PRINTING HERE");
+  return yamlStringToJson(yamlString);
+}
+
+module.exports = {
+  yamlStringToJson,
+  yamlFileToJson,
+};
